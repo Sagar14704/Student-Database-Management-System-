eb592ae8efcfcd844419f7c0edf14da2eb34d0e8
NANDURI VENKATA SAGAR SWAMY <94947990+Sagar14704@users.noreply.github.com>
2024-09-30 19:24:11 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,29 @@
+package model;
+
+	public class student {
+	    private int studentId;
+	    private String name;
+	    private int age;
+	    private String course;
+	    private double feesBalance;
+	    private double marks;
+
+	    // Getters and Setters
+	    public int getStudentId() { return studentId; }
+	    public void setStudentId(int studentId) { this.studentId = studentId; }
+
+	    public String getName() { return name; }
+	    public void setName(String name) { this.name = name; }
+
+	    public int getAge() { return age; }
+	    public void setAge(int age) { this.age = age; }
+
+	    public String getCourse() { return course; }
+	    public void setCourse(String course) { this.course = course; }
+
+	    public double getFeesBalance() { return feesBalance; }
+	    public void setFeesBalance(double feesBalance) { this.feesBalance = feesBalance; }
+
+	    public double getMarks() { return marks; }
+	    public void setMarks(double marks) { this.marks = marks; }
+	}
